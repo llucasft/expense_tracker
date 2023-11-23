462b6d2b9b18062ea6f8db9e850ed0819370d855
Lucas Ferreira <lucasfteo@gmail.com>
2023-11-23 10:23:30 -0300

    feat: implement expense class model and initialized app SQLite db

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    expense_name = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def add():
     return render_template('add.html')
+
+@app.route('/addexpense', methods=['POST'])
+def add_expense():
+    date = request.form['date']
+    expense_name = request.form['expensename']
+    amount = request.form['amount']
+    category = request.form['category']
+    print(date + ' ' + expense_name + ' ' + amount + ' ' + category)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
